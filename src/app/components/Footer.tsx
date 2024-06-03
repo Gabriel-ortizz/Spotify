e171998127ecf7653221fe1760e6c1575add78b7
Gabriel.ortizz <gabrielmd0graus@gmail.com>
2024-06-03 18:31:35 -0300

    refactory:footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -13,8 +13,8 @@ return(
   </div>
   <div className='flex flex-col items-center'>
   <div className='flex items-center gap-4'>
- <Shuffle size={20} className='text-zinc-200' />
- <SkipBack size={20} className='text-zinc-200' />
+ <Shuffle size={20} className='text-zinc-300' />
+ <SkipBack size={20} className='text-zinc-300' />
  <button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black'>
         <Play />
       </button>
